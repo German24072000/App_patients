0ce10fed9e162f4c2209999f5e00b02861f38f26
German24072000 <german24lara07@gmail.com>
2025-04-24 19:05:03 +0200

    feat add formgroup and add function of submit

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  loginForm = new FormGroup({
+    user: new FormControl('',Validators.required),
+    password: new FormControl('', Validators.required)
+  })
+  ngOnInit(): void {
+  }
+
+  onLogin() {
+    console.log(this.loginForm.value);
+    
+    
+  }
 
 }
