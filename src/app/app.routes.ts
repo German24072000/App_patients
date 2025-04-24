0b2e40a287a1f64563dd8a87b8f78e2ef7e584f8
German24072000 <german24lara07@gmail.com>
2025-04-24 16:44:07 +0200

    feat add routes for pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import {LoginComponent} from './views/login/login.component';
+import {DashboardComponent} from './views/dashboard/dashboard.component';
+import {EditPatientComponent} from './views/edit-patient/edit-patient.component';
+import {NewPatientComponent} from './views/new-patient/new-patient.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch:'full'
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'new-patient',
+        component: NewPatientComponent
+    },
+    {
+        path: 'edit-patient',
+        component: EditPatientComponent
+    },
+];
