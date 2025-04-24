983567a2d345842b4a973c753b957495d2465f16
German24072000 <german24lara07@gmail.com>
2025-04-24 16:41:25 +0200

    feat create pages component and api service

---
+++ src/app/views/new-patient/new-patient.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-patient',
+  imports: [],
+  templateUrl: './new-patient.component.html',
+  styleUrl: './new-patient.component.scss'
+})
+export class NewPatientComponent {
+
+}
