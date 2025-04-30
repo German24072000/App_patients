467b0959727c42cf7bc8689194c1374bd8dace6b
German24072000 <german24lara07@gmail.com>
2025-04-30 16:09:58 +0200

    feat add table with data - redirections to pages new-patient and edit-patient

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 
 import { ApiService} from '../../services/api/api.service';
+import {Router} from '@angular/router';
+
+import { ListPatientsI } from '../../models/listpatients.interface';
+
 @Component({
   selector: 'app-dashboard',
   imports: [HeaderComponent, FooterComponent],
@@ -11,12 +15,15 @@ import { ApiService} from '../../services/api/api.service';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private apiService:ApiService) {}
+  patients = signal<ListPatientsI[]>([]);
+
+  constructor(private apiService:ApiService, private router: Router) {}
 
   ngOnInit(): void {
     this.apiService.getAllPatients(1).subscribe({
       next: (data) => {
         console.log(data);
+        this.patients.set(data);
         
       },
       error: (err) => {
@@ -24,6 +31,15 @@ export class DashboardComponent implements OnInit {
         
       }
     });
+  }
+
+  editPatient(patient:ListPatientsI) {
+    this.router.navigate(['edit-patient', patient.idPatient])
+  }
+
+  newPatient() {
+    this.router.navigate(['new-patient'])
+    
   }
 
 }
